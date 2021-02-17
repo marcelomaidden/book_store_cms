a4082bdae01c8d691cb6b66fbc83fc433696b5ae
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-02-17 16:21:06 -0300

    Creates Book component

---
+++ src/components/Book.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default Book = ({ book }) => {
+  return (
+    <>
+      <td>book.id</td>
+      <td>book.title</td>
+      <td>book.category</td>
+    </>
+  )
+}
