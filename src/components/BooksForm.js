b3c68031396b91acc058db63c00872618064f03e
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-02-17 19:53:25 -0300

    Adds state to BooksForm component

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CATEGORIES = [
   { key: 0, name: 'Action' },
@@ -10,28 +10,41 @@ const CATEGORIES = [
   { key: 6, name: 'Sci-Fi' },
 ];
 
-const BooksForm = () => (
-  <form className="row g-3">
-    <div className="col-md-12">
-      <label htmlFor="inputTitle" className="form-label">
-        Title
-        <input type="text" className="form-control" id="inputTitle" />
-      </label>
-    </div>
-    <div className="col-md-12">
-      <label htmlFor="category" className="form-label">
-        Category
-        <select name="category" className="form-select" id="category">
-          {
-            CATEGORIES.map(({ key, name }) => <option value={name} key={key}>{name}</option>)
-          }
-        </select>
-      </label>
-    </div>
-    <div className="col-md-12">
-      <button type="button" className="btn btn-primary">Create book</button>
-    </div>
-  </form>
-);
+const BooksForm = () => {
+  const [book, setBook] = useState({ title: '', category: '' });
+
+  const handleChange = e => {
+    if (e.target.id === 'inputTitle') {
+      setBook({ ...book, title: e.target.value });
+    } else {
+      setBook({ ...book, category: e.target.value });
+    }
+  };
+
+  return (
+    <form className="row g-3">
+      <div className="col-md-12">
+        { `${book.title} - ${book.category}` }
+        <label htmlFor="inputTitle" className="form-label">
+          Title
+          <input type="text" onChange={handleChange} className="form-control" id="inputTitle" />
+        </label>
+      </div>
+      <div className="col-md-12">
+        <label htmlFor="category" className="form-label">
+          Category
+          <select name="category" onChange={handleChange} className="form-select" id="category">
+            {
+              CATEGORIES.map(({ key, name }) => <option value={name} key={key}>{name}</option>)
+            }
+          </select>
+        </label>
+      </div>
+      <div className="col-md-12">
+        <button type="button" className="btn btn-primary">Create book</button>
+      </div>
+    </form>
+  );
+};
 
 export default BooksForm;
