b173900ee770c88fcc36edd8571b8981024c529e
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-02-18 20:13:48 -0300

    Passes handleFilter from BookList to CategoryFilter

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { CHANGE_FILTER, CHANGE_VISIBILITY } from '../actions';
 
 export const CATEGORIES = [
   { key: 0, name: 'Action' },
@@ -13,36 +11,24 @@ export const CATEGORIES = [
   { key: 6, name: 'Sci-Fi' },
 ];
 
-const CategoryFilter = ({ changeFilter, changeVisibility }) => {
-  const handleFilter = e => {
-    changeFilter(e.target.value);
-    changeVisibility(e.target.value);
-  };
-  return (
-    <label htmlFor="filterCategories" className="form-label">
-      Filter category
-      <select name="filterCategories" onChange={handleFilter} id="filterCategories" className="form-select">
-        <option value="All">All</option>
-        {
-          CATEGORIES.map(category => (
-            <option value={category.name} key={category.key}>
-              {category.name}
-            </option>
-          ))
-        }
-      </select>
-    </label>
-  );
-};
+const CategoryFilter = ({ handleFilter }) => (
+  <label htmlFor="filterCategories" className="form-label">
+    Filter category
+    <select name="filterCategories" onChange={handleFilter} id="filterCategories" className="form-select">
+      <option value="All">All</option>
+      {
+        CATEGORIES.map(category => (
+          <option value={category.name} key={category.key}>
+            {category.name}
+          </option>
+        ))
+      }
+    </select>
+  </label>
+);
 
 CategoryFilter.propTypes = {
-  changeFilter: PropTypes.func.isRequired,
-  changeVisibility: PropTypes.func.isRequired,
+  handleFilter: PropTypes.func.isRequired,
 };
 
-const mapDispatchToProps = dispatch => ({
-  changeFilter: filter => (dispatch({ type: CHANGE_FILTER, filter })),
-  changeVisibility: filter => (dispatch({ type: CHANGE_VISIBILITY, filter })),
-});
-
-export default connect(null, mapDispatchToProps)(CategoryFilter);
+export default CategoryFilter;
--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -3,18 +3,33 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Book from '../components/Book';
 import CategoryFilter from '../components/CategoryFilter';
-import { deleteBook } from '../actions';
+import {
+  CHANGE_FILTER,
+  CHANGE_VISIBILITY,
+  deleteBook,
+} from '../actions';
 
-const BookList = ({ books, delete: handleRemoveBook }) => (
-  <div>
-    <CategoryFilter />
-    {
-      books.map(
-        book => <Book book={book} key={book.id} delete={handleRemoveBook} />,
-      )
-    }
-  </div>
-);
+const BookList = ({
+  books,
+  delete: handleRemoveBook,
+  handleChangeFilter,
+  handleVisibility,
+}) => {
+  const handleFilter = e => {
+    handleChangeFilter(e.target.value);
+    handleVisibility(e.target.value);
+  };
+  return (
+    <div>
+      <CategoryFilter handleFilter={handleFilter} />
+      {
+        books.map(
+          book => <Book book={book} key={book.id} delete={handleRemoveBook} />,
+        )
+      }
+    </div>
+  );
+};
 
 BookList.propTypes = {
   books: PropTypes.arrayOf(
@@ -25,12 +40,16 @@ BookList.propTypes = {
     }),
   ).isRequired,
   delete: PropTypes.func.isRequired,
+  handleVisibility: PropTypes.func.isRequired,
+  handleChangeFilter: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({ books: state.books });
 
 const mapDispatchToProps = dispatch => ({
   delete: book => dispatch(deleteBook(book)),
+  handleChangeFilter: filter => (dispatch({ type: CHANGE_FILTER, filter })),
+  handleVisibility: filter => (dispatch({ type: CHANGE_VISIBILITY, filter })),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(BookList);
