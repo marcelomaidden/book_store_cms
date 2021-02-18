0365b425c231a339b9d622196212dc760ac108b1
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-02-18 19:18:14 -0300

    Adds style to the books-panel

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../stylesheets/Book.css';
 
 const Book = ({ book, delete: handleRemoveBook }) => (
-  <table className={`${book.visible ? 'table' : 'd-none'}`}>
-    <tr>
-      <td className="col-3">{book.id}</td>
-      <td className="col-3">{book.title}</td>
-      <td className="col-3">{book.category}</td>
-      <td className="col-3">
-        <button type="button" onClick={() => handleRemoveBook(book)}>
-          <i className="fa fa-trash" />
-        </button>
-      </td>
-    </tr>
-  </table>
+  <div className={`${book.visible ? 'table book-panel' : 'd-none'}`}>
+    <div className="book-info">
+      <div className="book-category">
+        {book.category}
+      </div>
+      <div className="book-title">{book.title}</div>
+      <div className="book-id">
+        {`Book ID: ${book.id}`}
+      </div>
+      <div className="actions">
+        <span>Comments</span>
+        <span>Remove</span>
+        <span>Edit</span>
+      </div>
+    </div>
+    <i className="Oval-2" />
+    <div className="book-info">
+      <span className="percentage">64%</span>
+      <span className="completed">Completed</span>
+    </div>
+    <div>
+      <button type="button" onClick={() => handleRemoveBook(book)}>
+        Remove book
+        <i className="fa fa-trash" />
+      </button>
+    </div>
+  </div>
 );
 
 Book.propTypes = {
