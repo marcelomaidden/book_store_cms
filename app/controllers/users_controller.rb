b9142acf767ffcefdca0b0cb65af7d5c28b72db5
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-02-22 19:11:27 -0300

    Creates Users' index, create and sign_in using JWT auth

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    
+    render json: @users
+  end
+
+  def create
+    @user=User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def sign_in
+    user=User.find_by(email: login_params[:email])
+    
+    if user && user.authenticate(login_params[:password])
+      render json: JsonWebToken.encode(user_id: user.id)
+    else
+      render json: { message: "Invalid credentials" }
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
+  def login_params
+    params.require(:user).permit(:email, :password)
+  end
+end
