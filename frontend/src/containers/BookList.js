53561b83ad121ae7869ef8701915cdcebabf6f91
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-02-23 14:40:41 -0300

    Removes build folder from frontend

--- frontend/src/containers/BookList.js
+++ frontend/src/containers/BookList.js
@@ -4,15 +4,22 @@ import { connect } from 'react-redux';
 import Book from '../components/Book';
 import { deleteBook } from '../actions';
 
-const BookList = ({ books, delete: handleRemoveBook }) => (
-  <div>
-    {
-      books.map(
-        book => <Book book={book} key={book.id} delete={handleRemoveBook} />,
-      )
-    }
-  </div>
-);
+const BookList = ({ books, delete: handleRemoveBook }) => {
+  const users = async () => {
+    return await fetch('http://localhost:3000/users')
+    .then(data => data.json())
+    .then(result => console.log(result) );
+  };
+  users();
+  return (
+    <div>
+      {
+        books.map(
+          book => <Book book={book} key={book.id} delete={handleRemoveBook} />,
+        )
+      }
+    </div>
+)};
 
 BookList.propTypes = {
   books: PropTypes.arrayOf(
