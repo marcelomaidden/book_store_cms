7c5a9d53a85b2c809fee484b406b44d1079af84e
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-02-24 19:28:40 -0300

    Creates Category and Book models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_205145) do
+ActiveRecord::Schema.define(version: 2021_02_24_222730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "author"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_books_on_category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,4 +38,5 @@ ActiveRecord::Schema.define(version: 2021_02_22_205145) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "books", "categories"
 end
