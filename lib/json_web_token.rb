cd702d0a61969b908b6ea07a2834dca7860d5670
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-02-26 19:15:54 -0300

    Changes JWT token auth

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,12 +2,11 @@ class JsonWebToken
   class << self
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      JWT.encode(payload, nil, 'none')
     end
 
     def decode(token)
-      body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
-      HashWithIndifferentAccess.new body
+      body = JWT.decode(token, nil, false)
 
     rescue
       nil
